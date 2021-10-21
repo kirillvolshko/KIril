d2f27741aaabab27945dfc8b984597746a82d167
kirillvolshko <92945850+kirillvolshko@users.noreply.github.com>
2021-10-21 21:00:53 +0300

    ДЗ

---
+++ animal/src/animal.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val dolphin: animal1=animal1()
+    val bird: animal1=animal1()
+    val dog: animal1=animal1()
+
+
+            dolphin.dolphin = readLine().toString()
+            bird.bird = readLine().toString()
+            dog.dog = readLine().toString()
+
+
+            println(dolphin.dolphin)
+            println(bird.bird)
+            println(dog.dog)
+
+}
---
+++ animal/src/animal1.kt
@@ -0,0 +1,6 @@
+class animal1() {
+
+    var dolphin: String = "Undefined"
+    var bird: String = "Undefined"
+    var dog: String = "Undefined"
+}
